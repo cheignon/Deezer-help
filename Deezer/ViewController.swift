a5007dcdf5372ee772b3e4b6e32aa950c8b8484a
Holimotion <developer@holimotion.com>
2015-11-04 16:19:34 +0100

    Initial Commit

---
+++ Deezer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deezer
+//
+//  Created by Dorian Cheignon on 04/11/2015.
+//  Copyright © 2015 Dorian Cheignon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
